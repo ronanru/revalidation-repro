51a22788c047c5389de8aeb55855d74f5da82e86
Matvey Ryabchikov <35634442+ronanru@users.noreply.github.com>
2024-01-11 22:46:10 +0300

    fix /

--- src/app/route.ts
+++ src/app/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const GET = (req: Request) =>
-  NextResponse.redirect(new URL(req.url, "/test"));
+export const GET = (req: NextRequest) =>
+  NextResponse.redirect(new URL("/test", req.nextUrl));
